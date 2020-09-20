2c20a317de77b91fdafe2c3e427dc0fe388cf01a
Olga.L <olg200492@gmail.com>
2020-09-20 11:39:49 +0300

    Change files name

---
+++ GIF-randomizer/script.js
@@ -0,0 +1,5 @@
+let apikey = 'XGFI395wJ4btleHm5h6PlxhetZ9hOIrF';
+document.getElementById("button").addEventListener("click", function () {
+  alert("hello");
+  //validation code to see State field is mandatory.
+});
